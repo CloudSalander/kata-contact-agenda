98b7ece2019ba8e24a88e4096e1042ccdd30ca47
CloudSalander <ruben.as532@gmail.com>
2025-12-03 09:54:01 +0100

    Now with value objects

--- classes/Contact.php
+++ classes/Contact.php
@@ -1,11 +1,16 @@
 <?php 
 
+include('value-objects/Email.php');
+include('value-objects/PhoneNumber.php');
+
 class Contact {
     //todo: EMAIL AND PHONE validations
     const INPUT_NAME = "Enter name";
     const INPUT_SURNAME = "Enter surname";
     const INPUT_EMAIL = "Enter email";
     const INPUT_PHONE = "Enter phone number";
+    const INPUT_EMAIL_ERROR = "Please, enter valid email";
+    const INPUT_PHONE_ERROR = "Please, enter valid phone number";
 
     private string $name;
     private string $surname;
@@ -15,25 +20,42 @@ class Contact {
     public function __construct() {
         $this->name = readline(self::INPUT_NAME);
         $this->surname = readline(self::INPUT_SURNAME);
-        $this->email = readline(self::INPUT_EMAIL);
-        $this->phone = readline(self::INPUT_PHONE);
+        $this->enterPhone();
+        $this->enterEmail();
+        
+        //$this->phone = new PhoneNumber(readline(self::INPUT_PHONE));
     }
 
-    public function getName(): string {
-        return $this->name;
-    }
 
     public function getSurname(): string {
         return $this->surname;
     }
 
 
-    public function getEmail(): string {
-        return $this->email;
+    private function enterEmail(): void {
+        while(true) {
+            $email = readline(self::INPUT_EMAIL);
+            try{
+                $this->email = new Email($email);
+                break;
+            }
+            catch(InvalidArgumentException $e) {
+               echo self::INPUT_EMAIL_ERROR.PHP_EOL;
+            }
+        }
     }
 
-    public function getPhone(): string {
-        return $this->phone;
+    private function enterPhone(): void {
+        while(true) {
+            $phone = readline(self::INPUT_PHONE);
+            try{
+                $this->phone = new PhoneNumber($phone);
+                break;
+            }
+            catch(InvalidArgumentException $e) {
+               echo self::INPUT_PHONE_ERROR.PHP_EOL;
+            }
+        }
     }
 
     public function __toString() {
